924bac7f3f0fba4d3f9bdb681e250a01550e5d32
GraceUmutesi <tesigrace98@gmail.com>
2019-09-30 14:39:24 +0200

    deploy to heroku

--- manage.py
+++ manage.py
@@ -1,12 +1,11 @@
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
 from flask_script import Manager,Server
-# from flask_login import LoginManager
 from app.models import Writer
 
 # Creating app instance
-# app = create_app('development')
 app = create_app('development')
+# app = create_app('development')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
